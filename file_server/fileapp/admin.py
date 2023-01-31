94354dbfdd5055a443ba11eacb672ef30f60a424
d-danso <117347670+d-danso@users.noreply.github.com>
2023-01-31 10:53:53 +0000

    Delete file_server directory

--- file_server/fileapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from fileapp.models import File
-
-class FileAdmin(admin.ModelAdmin):
-    list_display = ('title', 'downloads','emails_sent')
-
-admin.site.register(File, FileAdmin)
-
--- file_server/fileapp/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-class File(models.Model):
-    title = models.CharField(max_length=255)
-    description = models.TextField()
-    file = models.FileField(upload_to='')
-    downloads = models.IntegerField(default=0)
-    emails_sent = models.IntegerField(default=0)
